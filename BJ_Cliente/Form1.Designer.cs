85ca562a1fa725e9b7e0ebf39ef5152b56784866
Gonzalo Grisafi <gonzagrisa@gmail.com>
2018-11-25 18:30:50 -0300

    Add files via upload

--- BJ_Cliente/Form1.Designer.cs
+++ BJ_Cliente/Form1.Designer.cs
@@ -28,43 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnOtra = new System.Windows.Forms.Button();
-            this.btnPlantarse = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // btnOtra
-            // 
-            this.btnOtra.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOtra.Location = new System.Drawing.Point(129, 179);
-            this.btnOtra.Name = "btnOtra";
-            this.btnOtra.Size = new System.Drawing.Size(259, 112);
-            this.btnOtra.TabIndex = 0;
-            this.btnOtra.Text = "Pedir Otra";
-            this.btnOtra.UseVisualStyleBackColor = true;
-            this.btnOtra.Click += new System.EventHandler(this.btnOtra_Click);
-            // 
-            // btnPlantarse
-            // 
-            this.btnPlantarse.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPlantarse.Location = new System.Drawing.Point(445, 179);
-            this.btnPlantarse.Name = "btnPlantarse";
-            this.btnPlantarse.Size = new System.Drawing.Size(259, 112);
-            this.btnPlantarse.TabIndex = 1;
-            this.btnPlantarse.Text = "Plantarse";
-            this.btnPlantarse.UseVisualStyleBackColor = true;
-            this.btnPlantarse.Click += new System.EventHandler(this.btnPlantarse_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(864, 482);
-            this.Controls.Add(this.btnPlantarse);
-            this.Controls.Add(this.btnOtra);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Shown += new System.EventHandler(this.Form1_Shown);
-            this.ResumeLayout(false);
+			this.components = new System.ComponentModel.Container();
+			this.btnOtra = new System.Windows.Forms.Button();
+			this.btnPlantarse = new System.Windows.Forms.Button();
+			this.timerListen = new System.Windows.Forms.Timer(this.components);
+			this.txtCartaRecibida = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.lblPuntos = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnOtra
+			// 
+			this.btnOtra.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnOtra.Location = new System.Drawing.Point(222, 343);
+			this.btnOtra.Margin = new System.Windows.Forms.Padding(2);
+			this.btnOtra.Name = "btnOtra";
+			this.btnOtra.Size = new System.Drawing.Size(130, 58);
+			this.btnOtra.TabIndex = 0;
+			this.btnOtra.Text = "Pedir Otra";
+			this.btnOtra.UseVisualStyleBackColor = true;
+			this.btnOtra.Click += new System.EventHandler(this.btnOtra_Click);
+			// 
+			// btnPlantarse
+			// 
+			this.btnPlantarse.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnPlantarse.Location = new System.Drawing.Point(380, 343);
+			this.btnPlantarse.Margin = new System.Windows.Forms.Padding(2);
+			this.btnPlantarse.Name = "btnPlantarse";
+			this.btnPlantarse.Size = new System.Drawing.Size(130, 58);
+			this.btnPlantarse.TabIndex = 1;
+			this.btnPlantarse.Text = "Plantarse";
+			this.btnPlantarse.UseVisualStyleBackColor = true;
+			this.btnPlantarse.Click += new System.EventHandler(this.btnPlantarse_Click);
+			// 
+			// timerListen
+			// 
+			this.timerListen.Interval = 1000;
+			this.timerListen.Tick += new System.EventHandler(this.timerListen_Tick);
+			// 
+			// txtCartaRecibida
+			// 
+			this.txtCartaRecibida.Location = new System.Drawing.Point(222, 319);
+			this.txtCartaRecibida.Margin = new System.Windows.Forms.Padding(2);
+			this.txtCartaRecibida.Name = "txtCartaRecibida";
+			this.txtCartaRecibida.Size = new System.Drawing.Size(290, 20);
+			this.txtCartaRecibida.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(219, 303);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(72, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Carta recibida";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(84, 23);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(63, 20);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Puntos:";
+			// 
+			// lblPuntos
+			// 
+			this.lblPuntos.AutoSize = true;
+			this.lblPuntos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPuntos.Location = new System.Drawing.Point(153, 23);
+			this.lblPuntos.Name = "lblPuntos";
+			this.lblPuntos.Size = new System.Drawing.Size(18, 20);
+			this.lblPuntos.TabIndex = 5;
+			this.lblPuntos.Text = "0";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(749, 412);
+			this.Controls.Add(this.lblPuntos);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtCartaRecibida);
+			this.Controls.Add(this.btnPlantarse);
+			this.Controls.Add(this.btnOtra);
+			this.Margin = new System.Windows.Forms.Padding(2);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Shown += new System.EventHandler(this.Form1_Shown);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -72,6 +129,11 @@
 
         private System.Windows.Forms.Button btnOtra;
         private System.Windows.Forms.Button btnPlantarse;
-    }
+        private System.Windows.Forms.Timer timerListen;
+        private System.Windows.Forms.TextBox txtCartaRecibida;
+        private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label lblPuntos;
+	}
 }
 
--- BJ_Cliente/Form1.cs
+++ BJ_Cliente/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BlackJack.Properties;
+using Cliente_Servidor;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,8 +14,17 @@ namespace BJ_Cliente
 {
     public partial class Form1 : Form
     {
+        Servidor server;
         Cliente cliente;
         Login ventanaLogin;
+		Juego juego = new Juego(15);
+
+		//Para las posiciones de las Cartas
+		int x = 100;
+		int y = 150;
+		int i = 1;
+		List<PictureBox> cartas = new List<PictureBox>();
+
         public Form1()
         {
             InitializeComponent();
@@ -21,9 +32,76 @@ namespace BJ_Cliente
             ventanaLogin = new Login();
             ventanaLogin.Show();
             ventanaLogin.enterPresionado += new Login.ElegirNombre(SetNombre);
+            server = new Servidor();
+            server.objetoRecibido += new Servidor.Recibido(ObjetoRecibido);
+            server.Start(6666);
+            timerListen.Start();
         }
 
-        private void SetNombre(string n)
+        private void ObjetoRecibido(Respuesta respuesta)
+        {
+            if (txtCartaRecibida.InvokeRequired)
+            {
+                txtCartaRecibida.Invoke(new MethodInvoker(delegate { txtCartaRecibida.Text = respuesta.carta.Nombre; }));
+            }
+
+			if (!juego.SumarPuntos(txtCartaRecibida.Text))
+				EliminarCartas();
+			else
+			{
+				lblPuntos.Text = juego.Puntos.ToString();
+				AgregarCarta();
+			}
+		}
+
+		#region AgregarCartas
+		private void AgregarCarta()
+		{
+			if (this.InvokeRequired)
+			{
+				this.Invoke(new Action(() =>
+				{
+					PictureBox pictureBox = new PictureBox();
+
+					pictureBox.Location = new System.Drawing.Point(x, y);
+					pictureBox.Name = "pictureBox" + i;
+					pictureBox.Size = new System.Drawing.Size(100, 150);
+					pictureBox.TabIndex = 4;
+					pictureBox.BackColor = Color.White;
+					pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+					pictureBox.TabStop = false;
+
+					CambiarImagen(txtCartaRecibida.Text, pictureBox);
+					this.Controls.Add(pictureBox);
+					pictureBox.BringToFront();
+					cartas.Add(pictureBox);
+					x += 25;
+				}));
+			}
+		}
+		
+        private void CambiarImagen(string nombre, PictureBox picture)
+		{
+			if (Char.IsDigit(nombre[0])){
+				nombre = " " + nombre;
+			}
+			nombre=nombre.Replace(' ', '_');
+			object O = Resources.ResourceManager.GetObject(nombre);
+			picture.Image = O as Image;
+		}
+		#endregion
+
+		private void EliminarCartas()
+		{
+			foreach (PictureBox carta in cartas)
+			{
+				carta.Dispose();
+			}
+		}
+
+
+
+		private void SetNombre(string n)
         {
             cliente.SetearClase(true,n);
             ventanaLogin.Close();
@@ -33,18 +111,23 @@ namespace BJ_Cliente
         private void btnOtra_Click(object sender, EventArgs e)
         {
             cliente.SetearClase(true);
-            cliente.Start();
+            cliente.Start(5555);
         }
 
         private void btnPlantarse_Click(object sender, EventArgs e)
         {
             cliente.SetearClase(false);
-            cliente.Start();
+            cliente.Start(5555);
         }
 
         private void Form1_Shown(object sender, EventArgs e)
         {
             this.Hide();
+        }
+
+        private void timerListen_Tick(object sender, EventArgs e)
+        {
+            server.EsperarRespuesta();
         }
     }
 }
